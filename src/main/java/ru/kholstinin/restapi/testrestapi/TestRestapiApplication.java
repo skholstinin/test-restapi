779cee24f4c846f9df37bbd0d4733419f6df707a
Sergey <s.kholstinin@yandex.ru>
2020-02-06 16:08:35 +0300

    Initial comit

---
+++ src/main/java/ru/kholstinin/restapi/testrestapi/TestRestapiApplication.java
@@ -0,0 +1,13 @@
+package ru.kholstinin.restapi.testrestapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TestRestapiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TestRestapiApplication.class, args);
+    }
+
+}
---
+++ src/test/java/ru/kholstinin/restapi/testrestapi/TestRestapiApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.kholstinin.restapi.testrestapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TestRestapiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
